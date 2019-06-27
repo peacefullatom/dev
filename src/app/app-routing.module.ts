6e6e4fc8429abd20712d042f736fb08e0ecf89c3
Yuriy Markov <peacefullatom@gmail.com>
2019-06-28 02:33:30 +0500

    added intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { introPath } from './const';
+import { IntroComponent } from './intro/intro.component';
+import { IntroModule } from './intro/intro.module';
 
-const routes: Routes = [{ path: '**', redirectTo: '' }];
+const routes: Routes = [
+  { path: introPath, component: IntroComponent },
+  { path: '', redirectTo: introPath, pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), IntroModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
