bf238fa3a04f52bf0a6489dc914595044e99ad78
Yuriy Markov <peacefullatom@gmail.com>
2019-06-29 00:10:13 +0500

    implemented navigation bar

---
+++ src/app/base/navigation/navigation.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation.component';
+
+@NgModule({
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent],
+  imports: [CommonModule, RouterModule]
+})
+export class NavigationModule {}
--- src/app/const.ts
+++ src/app/const.ts
@@ -1,1 +1,18 @@
+import { IntroComponent } from './intro/intro.component';
+import { IRoute } from './types';
+
+export const defaultPath = '/';
 export const introPath = 'intro';
+
+export const defaultRoute: IRoute = {
+  path: defaultPath,
+  skip: true,
+  label: '[DEV]'
+};
+
+export const appRoutes: IRoute[] = [
+  defaultRoute,
+  { path: introPath, component: IntroComponent, label: 'Intro' },
+  { path: '', redirectTo: introPath, pathMatch: 'full', skip: true },
+  { path: '**', redirectTo: '', skip: true }
+];
