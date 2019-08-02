3e9b4f286a3a37346a6ccb0a47cef8ac923bd4c4
Yuriy Markov <peacefullatom@gmail.com>
2019-08-03 00:14:27 +0500

    preview

--- src/app/page/prerequisites/prerequisites.component.ts
+++ src/app/page/prerequisites/prerequisites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { gitCliPath, parentPath, vsCodeExtensionsPath } from 'src/app/const';
 
 @Component({
   selector: 'app-prerequisites',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prerequisites.component.scss']
 })
 export class PrerequisitesComponent implements OnInit {
+  nodeJsUrl = 'https://nodejs.org';
+  yarnUrl = 'https://yarnpkg.com/';
+  eclipseUrl = 'http://eclipse.org/';
+  netBeansUrl = 'https://netbeans.org/';
+  webStormUrl = 'https://www.jetbrains.com/webstorm/';
+  codeUrl = 'https://code.visualstudio.com/';
+  atomUrl = 'https://atom.io/';
+  sublimeUrl = 'https://www.sublimetext.com/';
+  vimUrl = 'http://www.vim.org/';
+  gitUrl = 'https://git-scm.com/';
+  gitGuiUrl = 'https://git-scm.com/downloads/guis';
+  vsCodeExtensionsPath = [parentPath, vsCodeExtensionsPath];
+  gitCliPath = [parentPath, gitCliPath];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/page/prerequisites/prerequisites.module.ts
+++ src/app/page/prerequisites/prerequisites.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ExternalLinkModule } from 'src/app/core/ui/external-link/external-link.module';
 import { PrerequisitesRoutingModule } from './prerequisites-routing.module';
 import { PrerequisitesComponent } from './prerequisites.component';
 
 @NgModule({
   declarations: [PrerequisitesComponent],
   exports: [PrerequisitesComponent],
-  imports: [CommonModule, PrerequisitesRoutingModule]
+  imports: [CommonModule, PrerequisitesRoutingModule, ExternalLinkModule]
 })
 export class PrerequisitesModule {}
