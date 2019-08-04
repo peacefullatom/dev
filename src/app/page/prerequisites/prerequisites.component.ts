55aa15f2519ed240e83e8d4bcc821e97bf3dd80a
Yuriy Markov <peacefullatom@gmail.com>
2019-08-04 23:34:39 +0500

    added anchors to the prerequisites section

--- src/app/page/prerequisites/prerequisites.component.ts
+++ src/app/page/prerequisites/prerequisites.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { gitCliPath, parentPath, vsCodeExtensionsPath } from 'src/app/const';
 import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { gitCliPath, parentPath, vsCodeExtensionsPath } from 'src/app/const';
 
 @Component({
   selector: 'app-prerequisites',
@@ -8,8 +8,11 @@ import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./prerequisites.component.scss']
 })
 export class PrerequisitesComponent implements OnInit {
+  nodeJsAnchor = 'NodeJs';
   nodeJsUrl = 'https://nodejs.org';
+  yarnAnchor = 'Yarn';
   yarnUrl = 'https://yarnpkg.com/';
+  ideAnchor = 'IDE';
   eclipseUrl = 'http://eclipse.org/';
   netBeansUrl = 'https://netbeans.org/';
   webStormUrl = 'https://www.jetbrains.com/webstorm/';
@@ -17,9 +20,14 @@ export class PrerequisitesComponent implements OnInit {
   atomUrl = 'https://atom.io/';
   sublimeUrl = 'https://www.sublimetext.com/';
   vimUrl = 'http://www.vim.org/';
+  gitAnchor = 'git';
   gitUrl = 'https://git-scm.com/';
   gitGuiUrl = 'https://git-scm.com/downloads/guis';
+  angularAnchor = 'Angular';
+  angularUrl = 'https://angular.io';
+  bootstrapAnchor = 'Bootstrap';
   bootstrapUrl = 'https://getbootstrap.com/';
+  fontAwesomeAnchor = 'FontAwesome';
   fontAwesomeUrl = 'https://fontawesome.com/';
   fontAwesomeIconsUrl = 'https://fontawesome.com/icons';
   angularFontAwesomeUrl = 'https://github.com/FortAwesome/angular-fontawesome';
