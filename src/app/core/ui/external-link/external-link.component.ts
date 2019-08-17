2cc5218c5b479344bfdafdaa9c06ce7c94f8208b
Yuriy Markov <peacefullatom@gmail.com>
2019-08-18 01:17:30 +0500

    refactored font awesome constants

--- src/app/core/ui/external-link/external-link.component.ts
+++ src/app/core/ui/external-link/external-link.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
 
-import { iconSizeXs } from '../../const/icon';
+import { faIconSizeXs } from '../../const/icon';
 
 @Component({
   selector: 'app-external-link',
@@ -12,7 +12,7 @@ export class ExternalLinkComponent implements OnInit {
   @Input() href = '';
 
   faExternalLinkSquareAlt = faExternalLinkSquareAlt;
-  iconSizeXs = iconSizeXs;
+  faIconSizeXs = faIconSizeXs;
 
   constructor() {}
 
