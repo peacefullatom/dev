bc13c81a13892ad989d638b773d8e6dcd0d23e3d
Yuriy Markov <peacefullatom@gmail.com>
2019-09-07 04:53:12 +0500

    updated code component

--- src/app/core/ui/code/code.component.ts
+++ src/app/core/ui/code/code.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-code',
   templateUrl: './code.component.html',
   styleUrls: ['./code.component.scss']
 })
-export class CodeComponent implements OnInit {
-  @Input() value = '';
+export class CodeComponent implements OnInit, OnChanges {
+  /** the value will be stringified */
+  @Input() value: string;
+  /** the text will be inserted as is */
+  @Input() text: string;
+  /** the html will be inserted via insertAdjacentHTML */
+  @Input() html: string;
+  @ViewChild('code', { static: true }) codeRef: ElementRef<HTMLDivElement>;
 
   className = 'bg-dark text-light py-3 px-3 rounded';
+  content = false;
 
   constructor() {}
 
@@ -16,5 +23,24 @@ export class CodeComponent implements OnInit {
     if (this.value && typeof this.value !== 'string') {
       this.value = JSON.stringify(this.value);
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.html || changes.text || changes.value) {
+      this.content = !!this.html || !!this.text || !!this.value;
+      if (this.html && this.codeRef) {
+        this.codeRef.nativeElement.insertAdjacentHTML('afterbegin', this.html);
+      }
+    }
+  }
+
+  render(): string {
+    if (this.value) {
+      return this.value;
+    }
+    if (this.text) {
+      return this.text;
+    }
+    return '';
   }
 }
