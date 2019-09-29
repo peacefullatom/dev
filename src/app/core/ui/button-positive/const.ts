1506cb806068ec75af2f641c582b69336113206c
Yuriy Markov <peacefullatom@gmail.com>
2019-09-29 06:36:53 +0500

    added components button section

---
+++ src/app/core/ui/button-positive/const.ts
@@ -0,0 +1,1 @@
+export const buttonPositiveId = 'buttonPositive';
--- src/app/page/components/components-button/components-button.component.ts
+++ src/app/page/components/components-button/components-button.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faIcons } from '@fortawesome/free-solid-svg-icons';
 import { bsBtnDanger, bsBtnDark, bsBtnSizeLarge, bsBtnSizeSmall, bsBtnWarning } from 'src/app/core/bootstrap.const';
 import { faIconSize2x } from 'src/app/core/fa.const';
+import { buttonPositiveId } from 'src/app/core/ui/button-positive/const';
 import { SourceViewer } from 'src/app/core/ui/source-viewer/types';
 
 import { bootstrapUrl } from '../../const';
@@ -15,20 +16,18 @@ export class ComponentsButtonComponent implements OnInit {
   readonly faIcons = faIcons;
   readonly buttonSources: SourceViewer = { component: 'button', active: true };
   readonly buttonPositiveSources: SourceViewer = [
-    { singleComponent: 'buttonPositive', active: true },
-    { module: 'buttonPositive' }
+    { singleComponent: buttonPositiveId, active: true },
+    { module: buttonPositiveId }
   ];
   readonly bsBtnDark = bsBtnDark;
   readonly bsBtnWarning = bsBtnWarning;
   readonly bsBtnDanger = bsBtnDanger;
   readonly bsBtnSizeSmall = bsBtnSizeSmall;
   readonly bsBtnSizeLarge = bsBtnSizeLarge;
-
   readonly faIconSize2x = faIconSize2x;
-
   readonly bootstrapUrl = bootstrapUrl;
-
   readonly buttonCode = `<button></button>`;
+  readonly buttonPositiveExampleCode = `<app-button-positive (click)="agree()">Agree</app-button-positive>`;
 
   constructor() {}
 
